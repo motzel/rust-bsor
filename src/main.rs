2e1ee59b8a0821b0167e381709df2edce3530379
motzel <motzel@o2.pl>
2023-01-11 21:26:10 +0100

    chore: example update

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,5 @@ fn main() {
 
     let replay = Replay::load(br).unwrap();
 
-    let frames = replay.frames.get_vec();
-    if frames.len() > 0 {
-        println!("Count: {}, frames[0]={:#?}", frames.len(), frames[0]);
-    } else {
-        println!("No frames!");
-    }
+    println!("{:#?}", replay.info);
 }
